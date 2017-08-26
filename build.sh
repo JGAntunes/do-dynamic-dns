514a1ed94401465f2415fc9c647c906bf6215824
JGAntunes <j.goncalo.antunes@gmail.com>
2017-08-26 01:42:26 +0100

    update build script

--- build.sh
+++ build.sh
@@ -3,13 +3,19 @@
 set -ex
 
 DOCKERFILE="Dockerfile"
+RESTART="no"
+DETACHED="false"
 
 if [ "$PI" != "" ]; then DOCKERFILE="Dockerfile.rpi"; fi
+
+if [ "$RESTART" != "" ]; then RESTART="always"; fi
+
+if [ "$DETACHED" != "" ]; then DETACHED="true"; fi
 
 docker build --rm --tag do-dynamic-dns --file $DOCKERFILE .
 
 if [ "$RUN" != "" ]; then
-  docker run -e DOMAIN -e UPDATE_INTERVAL -e DO_TOKEN do-dynamic-dns
+  docker run -d=$DETACHED --restart=$RESTART -e DOMAIN -e UPDATE_INTERVAL -e DO_TOKEN do-dynamic-dns
 fi;
 
 set +ex
